c389a3b861025edc64ae9b0f2061142298829b22
labogosselin <labogosselin@gmail.com>
2015-09-24 13:21:05 -0400

    Added Smooth

--- SSP.m
+++ SSP.m
@@ -2,9 +2,10 @@ function zci=SSP(ci,ci_boot,sigma)
 %  Smooth, Standardize & Padd Cis
 i= load('images.mat');
 
-zci=SmoothCi(ci,sigma);
+sci=SmoothCi(ci,sigma);
+sci_boot=SmoothCi(ci_boot,sigma);
 
-zci = (ci - mean(ci_boot(i.masque==1))) / std(ci_boot(i.masque==1));
+zci = (sci - mean(sci_boot(i.masque==1))) / std(sci_boot(i.masque==1));
 
 zci= padarray(zci,[128 128], 'symmetric');
 zci= zci(128:255,128:255);
